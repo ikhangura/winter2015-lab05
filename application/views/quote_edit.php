da01aa59a9e59f6271c44b2484b74d678ac53c29
ikhangura <ikhangura@gmail.com>
2015-02-21 00:36:30 -0800

    A1 finished Signed-off-by:ikhangura <ikhangura@gmail.com>

---
+++ application/views/quote_edit.php
@@ -0,0 +1,12 @@
+
+       <div class="row">
+	<div class="errors" style="color: red; font-weight:bold; font-size:large">{message}</div>
+	<form action="/admin/confirm" method="post">
+		{fid}
+		{fwho}
+		{fmug}
+		{fwhat}
+		{fsubmit}
+	</form>
+</div>
+    
